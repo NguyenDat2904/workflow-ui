9bc8bdf81a4629577fd983718f93f89bcaaa6e92
MawatariTanaka <buinhatminh2005@gmail.com>
2023-12-01 22:10:22 +0700

    WF-10 đăng nhập bằng email

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import './Login.scss';
 
 function Login() {
-    return <div>Login</div>;
+    return (
+        <div id="login">
+            <form class="login-container">
+                <h3>
+                    Log in
+                    <p>With your WorkFlow account</p>
+                </h3>
+                <input type="text" id="username" name="username" placeholder="example@email.com" />
+                <input type="password" id="password" name="password" placeholder="Enter password" />
+                <button type="submit">Log in</button>
+                <p>OR</p>
+            </form>
+        </div>
+    );
 }
 
 export default Login;

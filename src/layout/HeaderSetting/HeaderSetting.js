8bf7c0015e93e01f3bc894376fa98ab6fbecfae9
Hung <vanhungnvh1712004@nvh1712004gmail.com>
2023-12-16 10:53:19 +0700

    wf-wf14

---
+++ src/layout/HeaderSetting/HeaderSetting.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import Button from '~/component/Buttton/Button';
+import classNames from 'classnames/bind';
+import { NavLink, Link } from 'react-router-dom';
+import style from './HeaderSetting.module.scss';
+import { DownIcon, HelpIcon, NotificationIcon, SettingIcon, ShuttleIcon, UserIcon } from '~/component/icon/icon';
+import { useLocation } from 'react-router-dom';
+import ModalProject from '~/pages/Projects/ModalProject/ModalProject';
+const cx = classNames.bind(style);
+
+function HeaderSetting() {
+    const location = useLocation();
+    // 1. State
+    const [toggleMenu, setToggleMenu] = useState({
+        yourWork: false,
+        project: false,
+        team: false,
+    });
+
+    // 3. Func
+    const handleToggle = (toggle) => {
+        switch (toggle) {
+            case 'project':
+                setToggleMenu((pre) => ({
+                    ...pre,
+                    project: !toggleMenu.project,
+                }));
+                break;
+            case 'yourWork':
+                setToggleMenu((pre) => ({
+                    ...pre,
+                    yourWork: !toggleMenu.yourWork,
+                }));
+                break;
+            case 'team':
+                setToggleMenu((pre) => ({
+                    ...pre,
+                    team: !toggleMenu.team,
+                }));
+                break;
+            default:
+                setToggleMenu({
+                    yourWork: false,
+                    project: false,
+                    team: false,
+                });
+        }
+    };
+    return (
+        <header className={cx('header-layout')}>
+            <nav>
+                <Button leftIcon={<ShuttleIcon />} backgroundNone noChildren to="/"></Button>
+                <div className={cx('list-menu')}>
+                    <div
+                        className={cx('menu', location.pathname === '/project' && 'active')}
+                        onClick={() => handleToggle('project')}
+                    >
+                        <Button
+                            rightIcon={<DownIcon />}
+                            backgroundNone
+                            className={cx(toggleMenu.project && 'toggle', 'text-blue')}
+                        >
+                            Projects
+                        </Button>
+                    </div>
+
+                    {toggleMenu.project && <ModalProject handleToggle={() => handleToggle('project')} />}
+                    <NavLink to={'/profile/security'}>
+                        <div className={cx('menu')} onClick={() => handleToggle('team')}>
+                            <Button backgroundNone>Security</Button>
+                        </div>
+                    </NavLink>
+                </div>
+            </nav>
+            <div className={cx('nav-right')}>
+                <div className={cx('nav-icon')}>
+                    <Button
+                        className={cx('button-icon')}
+                        noChildren
+                        backgroundNone
+                        borderRadius
+                        leftIcon={<NotificationIcon />}
+                    ></Button>
+                </div>
+                <div className={cx('nav-icon')}>
+                    <Button
+                        className={cx('button-icon', 'custom-button')}
+                        noChildren
+                        backgroundNone
+                        borderRadius
+                        leftIcon={<HelpIcon />}
+                    ></Button>
+                </div>
+                <div className={cx('nav-icon')}>
+                    <Button
+                        className={cx('button-icon')}
+                        noChildren
+                        backgroundNone
+                        borderRadius
+                        leftIcon={<SettingIcon />}
+                    ></Button>
+                </div>
+                <div className={cx('nav-icon')}>
+                    <Button
+                        className={cx('button-icon')}
+                        noChildren
+                        backgroundNone
+                        borderRadius
+                        leftIcon={<UserIcon />}
+                    ></Button>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default HeaderSetting;

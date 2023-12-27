2659675581b932836aac411e323f61b9b606e40c
nguyenbinh2003 <nguyenbinh191103@gmail.com>
2023-12-27 22:06:33 +0700

    feat-context-publicRoute-privateRoute

--- src/App.js
+++ src/App.js
@@ -58,59 +58,6 @@ function App() {
                   })}
                </Route>
             </Routes>
-
-            {/* <Routes>
-               {privateRoutes.map((route, index) => {
-                  const Page = route.component;
-                  let Layout = DefaultLayout;
-                  if (route.layout) {
-                     Layout = route.layout;
-                  } else if (route.layout === null) {
-                     Layout = Fragment;
-                  }
-
-                  return (
-                     <Route
-                        key={index}
-                        path={route.path}
-                        element={
-                           isAuthenticated ? (
-                              <Layout>
-                                 <Page />
-                              </Layout>
-                           ) : (
-                              <Navigate to="/login" />
-                           )
-                        }
-                     />
-                  );
-               })}
-               {publicRoutes.map((route, index) => {
-                  const Page = route.component;
-                  let Layout = DefaultLayout;
-                  if (route.layout) {
-                     Layout = route.layout;
-                  } else if (route.layout === null) {
-                     Layout = Fragment;
-                  }
-
-                  return (
-                     <Route
-                        key={index}
-                        path={route.path}
-                        element={
-                           isAuthenticated ? (
-                              <Navigate to="/project" />
-                           ) : (
-                              <Layout>
-                                 <Page />
-                              </Layout>
-                           )
-                        }
-                     />
-                  );
-               })}
-            </Routes> */}
          </div>
          <ToastContainer />
       </BrowserRouter>

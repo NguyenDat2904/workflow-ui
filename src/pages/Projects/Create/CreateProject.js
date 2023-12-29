c7a3700fb620555f6daa5f62c51083681044b51a
MawatariTanaka <buinhatminh2005@gmail.com>
2023-12-29 22:00:58 +0700

    Modify create project

--- src/pages/Projects/Create/CreateProject.js
+++ src/pages/Projects/Create/CreateProject.js
@@ -1,11 +1,14 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './CreateProject.scss';
+import classNames from 'classnames/bind';
+import style from './CreateProject.scss';
 import { Button, Form, Input } from '~/component/Inputs/Inputs';
 import { Card } from '~/component/cards/Cards';
 import { toast } from 'react-toastify';
 import { post } from '~/ultil/hpptRequest';
 import { UserContext } from '~/contexts/user/userContext';
+
+const cx = classNames.bind(style);
 
 export default function CreateProject() {
    const { GetListProject } = useContext(UserContext);
@@ -15,42 +18,49 @@ export default function CreateProject() {
    const user = JSON.parse(localStorage.getItem('user'));
    const handleCreateProject = async (e) => {
       e.preventDefault();
-      if (projectName && projectKey) {
-         const response = await post(
-            `/work/add-new-project/${user?._id}`,
-            {
-               nameProject: projectName,
-               codeProject: projectKey,
-            },
-            {
-               headers: {
-                  authorization: `${user.accessToken}`,
-                  refresh_token: `${user.refreshToken}`,
-               },
-            },
-         );
-         switch (response.status) {
-            case 200:
-               console.log(response);
-               GetListProject();
-               navigate('/project');
-               break;
-            case 400:
-               navigate('/login');
-               break;
-            case 401:
-               toast.error('Either the project name or project key already exists.');
-               break;
-            default:
-               toast.error('Something went wrong. Please try again later.');
-               break;
-         }
-         return;
-      }
+      console.log(e);
+      // if (projectName && projectKey) {
+      //    const response = await post(
+      //       `/work/add-new-project/${user?._id}`,
+      //       {
+      //          nameProject: projectName,
+      //          codeProject: projectKey,
+      //       },
+      //       {
+      //          headers: {
+      //             authorization: `${user.accessToken}`,
+      //             refresh_token: `${user.refreshToken}`,
+      //          },
+      //       },
+      //    );
+      //    switch (response.status) {
+      //       case 200:
+      //          console.log(response);
+      //          GetListProject();
+      //          navigate('/project');
+      //          break;
+      //       case 400:
+      //          navigate('/login');
+      //          break;
+      //       case 401:
+      //          toast.error('Either the project name or project key already exists.');
+      //          break;
+      //       default:
+      //          toast.error('Something went wrong. Please try again later.');
+      //          break;
+      //    }
+      //    return;
+      // }
    };
    return (
-      <Card className={'create-project-container'}>
-         <h3>Create a New Project</h3>
+      <Card className={cx('create-project-container')}>
+         <div>
+            <p className={cx('title')}>Create a New Project</p>
+            <p className={cx('subtitle')}>
+               Explore what's possible when you collaborate with your team. Edit project details anytime in project
+               settings.
+            </p>
+         </div>
          <Form onSubmit={(e) => handleCreateProject(e)}>
             <Input
                id="project-name"
@@ -69,7 +79,7 @@ export default function CreateProject() {
                placeholder="Project Key"
             />
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-               <Button onClick={() => navigate('/project')}>Cancel</Button>
+               <Button onClick={() => navigate('/project')}>Back to Project</Button>
                <Button
                   buttonStyle={projectName && projectKey ? 'filled' : 'disabled'}
                   type="submit"

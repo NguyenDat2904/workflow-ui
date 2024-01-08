87bb7215185bc9045f64e79ac3bcaeda75001f95
MawatariTanaka <buinhatminh2005@gmail.com>
2024-01-08 22:08:42 +0700

    Add svg

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -3,10 +3,31 @@ import classNames from 'classnames/bind';
 import style from './Board.scss';
 import { Button } from '../../component/Inputs/Inputs';
 import HeaderProject from '../BlackLog/HeaderProject/HeaderProject';
+import { Card } from '~/component/cards/Cards';
 
 const cx = classNames.bind(style);
 
+function Issue({ issueDetail, ...props }) {
+   console.log(issueDetail);
+   return (
+      <Card className={cx('task')} {...props}>
+         <p>{issueDetail.summary}</p>
+         <div className={cx('metadata')}>
+            <img src={`${issueDetail.assignee.img}`} alt="" />
+         </div>
+      </Card>
+   );
+}
+
 export default function Board() {
+   const sampleIssue = {
+      summary: 'Sub Issue',
+      assignee: {
+         name: 'Huy Van Hoang',
+         img: 'https://i.pravatar.cc/300',
+      },
+   };
+
    const rightSection = (
       <div className={cx('sprint-buttons')}>
          <Button buttonStyle="filled">Complete Sprint</Button>
@@ -23,6 +44,35 @@ export default function Board() {
                <div>In progress</div>
                <div>Review</div>
                <div>Done</div>
+            </div>
+            <div className={cx('task-display')}>
+               <div className={cx('main-task')}>Main Issue 1</div>
+               <div className={cx('sub-tasks-container')}>
+                  <div className={cx('sub-tasks')}>
+                     <Issue issueDetail={sampleIssue} />
+                     <Issue issueDetail={sampleIssue} />
+                     <Issue issueDetail={sampleIssue} />
+                     <Issue issueDetail={sampleIssue} />
+                  </div>
+                  <div className={cx('sub-tasks')}>
+                     <Issue issueDetail={sampleIssue} />
+                     <Issue issueDetail={sampleIssue} />
+                     <Issue issueDetail={sampleIssue} />
+                     <Issue issueDetail={sampleIssue} />
+                  </div>
+                  <div className={cx('sub-tasks')}>
+                     <Issue issueDetail={sampleIssue} />
+                     <Issue issueDetail={sampleIssue} />
+                     <Issue issueDetail={sampleIssue} />
+                     <Issue issueDetail={sampleIssue} />
+                  </div>
+                  <div className={cx('sub-tasks')}>
+                     <Issue issueDetail={sampleIssue} />
+                     <Issue issueDetail={sampleIssue} />
+                     <Issue issueDetail={sampleIssue} />
+                     <Issue issueDetail={sampleIssue} />
+                  </div>
+               </div>
             </div>
          </div>
       </div>

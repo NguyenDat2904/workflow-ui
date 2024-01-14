aedf185cb6145c4693187766a764b95d8fa4bfd5
Hung <vanhungnvh1712004@nvh1712004gmail.com>
2024-01-14 20:39:38 +0700

    fix

--- src/pages/Profile/viewAllListWork/viewAllListWork.js
+++ src/pages/Profile/viewAllListWork/viewAllListWork.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
-import TaskMaster from '../taskMaster/taskMaster';
 import { SearchIcon } from '~/component/icon/icon';
+import Pagination from '~/component/Pagination/Pagination';
 import WorkService from '~/services/work/workServices';
 import UserService from '~/services/user/userServices';
 import styles from './viewAllListWork.module.scss';
@@ -14,12 +14,10 @@ const ViewAllListWork = () => {
    const [dataListProject, setDataListProject] = useState([]);
    const [dataListWork, setDataListWork] = useState([]);
    const [dataUserProfile, setDataUserProfile] = useState({});
-   const [stylesView, setStylesView] = useState(true);
+   const [page, setPage] = useState(null);
    const [valueInput, setVaLueInput] = useState('');
-   const user = localStorage.getItem('user');
-   const parseuser = JSON.parse(user);
    const APIListProjetc = async () => {
-      const dataProject = await workServices.getListProject(parseuser?._id);
+      const dataProject = await workServices.getListProject();
       setDataListProject(dataProject.data.data);
       const APIuser = await userServices.getUserProfile({ deleteProject: false });
       setDataUserProfile(APIuser.data);
@@ -28,16 +26,15 @@ const ViewAllListWork = () => {
       APIListProjetc();
    }, []);
 
-   const hendleStylesView = (bool) => {
-      setStylesView(bool);
-   };
    const handleChangeInput = (e) => {
       setVaLueInput(e.target.value);
    };
    const handleSelect = async (e) => {
+      console.log(e.target.value);
       if (e.target.value !== '') {
-         const listWork = await workServices.getIssues(e.target.value);
+         const listWork = await workServices.getIssues(e.target.value, { limit: 5 });
          setDataListWork(listWork?.data?.dataListIssues);
+         setPage(listWork?.data?.totalPage);
       } else {
          setDataListWork([]);
       }
@@ -67,7 +64,6 @@ const ViewAllListWork = () => {
                   <SearchIcon />
                   <input
                      onChange={handleChangeInput}
-                     style={{ cursor: stylesView ? 'auto' : 'not-allowed' }}
                      className={cx('inputSearchTitle')}
                      type="text"
                      placeholder="Filter by title"
@@ -75,85 +71,66 @@ const ViewAllListWork = () => {
                </div>
             </div>
             <div className={cx('workOnAndviewed')}>
-               <p
-                  onClick={() => hendleStylesView(true)}
-                  style={{ color: stylesView ? '#0c66e4' : '#172b4d' }}
-                  className={cx('view')}
-               >
+               <p style={{ color: '#0c66e4' }} className={cx('view')}>
                   Worked on
-               </p>
-               <p
-                  onClick={() => hendleStylesView(false)}
-                  style={{ color: stylesView ? '#172b4d' : '#0c66e4', transition: 'all 1s' }}
-                  className={cx('view')}
-               >
-                  Taskmaster
                </p>
             </div>
             <div className={cx('bgrView')}>
-               <div
-                  style={{ marginLeft: stylesView ? '0' : '80px', transition: 'all 1s' }}
-                  className={cx('bgrViewChildren')}
-               ></div>
+               <div className={cx('bgrViewChildren')}></div>
             </div>
-            {stylesView === false ? (
-               <TaskMaster dataListWorkTaskMaster={dataListWork} />
-            ) : (
-               <div className={cx('implementerMenber')}>
-                  <p className={cx('project')}>Project</p>
-                  <div className={cx('viewWorkSelect')}>
-                     <div className={cx('nameProject')}>
-                        <img
-                           className={cx('imgFile')}
-                           src="https://cdn-icons-png.flaticon.com/512/124/124837.png"
-                           alt=""
-                        />
-                        <div className={cx('nameProjectRight')}>
-                           <p className={cx('nameUser')}>
-                              Name user : <span className={cx('nameUserspan')}>{dataUserProfile.name}</span>
-                           </p>
-                        </div>
-                     </div>
-                     <p>Your created: {dataUserProfile.createdAt?.slice(0, 10)}</p>
-                     <div className={cx('positionSelect')}>
-                        <div className={cx('select')}>
-                           <select className={cx('selectNameProject')} onChange={handleSelect} name="" id="">
-                              <option className={cx('opacity')} value="">
-                                 Project...
-                              </option>
-                              {dataListProject?.map((product) => {
-                                 return (
-                                    <option key={product?._id} value={product?._id}>
-                                       {product.nameProject}
-                                    </option>
-                                 );
-                              })}
-                           </select>
-                        </div>
+            <div className={cx('implementerMenber')}>
+               <p className={cx('project')}>Project</p>
+               <div className={cx('viewWorkSelect')}>
+                  <div className={cx('nameProject')}>
+                     <img
+                        className={cx('imgFile')}
+                        src="https://cdn-icons-png.flaticon.com/512/124/124837.png"
+                        alt=""
+                     />
+                     <div className={cx('nameProjectRight')}>
+                        <p className={cx('nameUser')}>
+                           Name user : <span className={cx('nameUserspan')}>{dataUserProfile.name}</span>
+                        </p>
                      </div>
                   </div>
-                  <p className={cx('project')}>List Work</p>
-
-                  {resultdataListWork?.map((product) => {
-                     return (
-                        <div key={product?._id} className={cx('viewWorkSelect')}>
-                           <div className={cx('nameProject')}>
-                              <img
-                                 className={cx('imgFile')}
-                                 src="https://media.istockphoto.com/id/1256489977/vector/tasks-check-checklist-blue-icon.jpg?s=612x612&w=0&k=20&c=dUctYWRSmMz1uiSFCCcJUKOyeoxVbvPuLugf8CLQiSo="
-                                 alt=""
-                              />
-                              <div className={cx('nameProjectRight')}>
-                                 <h6 className={cx('nameP')}>{product.summary}</h6>
-                                 <p className={cx('nameUser')}>{product.jobCode} go to maker sample</p>
-                              </div>
-                           </div>
-                           <p>Your created: {product.createdAt?.slice(0, 10)}</p>
-                        </div>
-                     );
-                  })}
+                  <p>Your created: {dataUserProfile.createdAt?.slice(0, 10)}</p>
+                  <div className={cx('positionSelect')}>
+                     <div className={cx('select')}>
+                        <select className={cx('selectNameProject')} onChange={handleSelect} name="" id="">
+                           <option className={cx('opacity')} value="">
+                              Project...
+                           </option>
+                           {dataListProject?.map((product) => {
+                              return (
+                                 <option key={product?._id} value={product?.codeProject}>
+                                    {product.nameProject}
+                                 </option>
+                              );
+                           })}
+                        </select>
+                     </div>
+                  </div>
                </div>
-            )}
+               <p className={cx('project')}>List Work</p>
+
+               {resultdataListWork?.map((product) => {
+                  return (
+                     <div key={product?._id} className={cx('viewWorkSelect')}>
+                        <div className={cx('nameProject')}>
+                           <img className={cx('imgFile')} src={product?.img} alt="" />
+                           <div className={cx('nameProjectRight')}>
+                              <h6 className={cx('nameP')}>{product.summary}</h6>
+                              <p className={cx('nameUser')}>go to maker sample</p>
+                           </div>
+                        </div>
+                        <p>Your created: {product.createdAt?.slice(0, 10)}</p>
+                     </div>
+                  );
+               })}
+            </div>
+            <div className={cx('page')}>
+               {dataListWork?.length > 0 &&<Pagination page={page} />}
+            </div>
          </div>
       </div>
    );
